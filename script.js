7b774b1dd96e1fa72ea2ef8c7c92aa62241bd02f
Akesh Yadav <85454169+Akesh0909@users.noreply.github.com>
2022-01-12 09:17:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-getUser("Akesh-Yadav");
+getUser("Akesh0909");
 
 async function getUser(username) {
     const resp = await fetch(APIURL + username);
